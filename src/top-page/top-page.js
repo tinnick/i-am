50bdc142349cbfd807c5fafc8a0b0b68d68a6f50
Julian Payne <tinnick@gmail.com>
2021-07-13 09:37:17 +0900

    base created

--- src/top-page/top-page.js
+++ src/top-page/top-page.js
@@ -1,3 +1,33 @@
 import './top-page.scss';
 
 import { rirekishoService } from '../shared/js';
+
+function loadContent() {
+    const rirekishoList = document.querySelector('#rirekisho-list');
+
+    if (!rirekishoList) {
+        console.warn('failed to load initlal contents');
+    }
+    
+    const documentList = rirekishoService.getAll();
+
+    if (!documentList || documentList.length) {
+        console.warn('no documents found');
+    }
+    
+    console.table(documentList);
+}
+
+function initApplication() {
+    loadContent();
+}
+
+function loadingLoop() {
+    if (document.readyState === 'complete') {
+        initApplication();
+    } else {
+        requestAnimationFrame(loadingLoop);
+    }
+};
+
+loadingLoop();
--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,7 @@ module.exports = () => {
     );
   } else {
     config.mode = "development";
+    config.devtool = "inline-source-map";
   }
   return config;
 };
